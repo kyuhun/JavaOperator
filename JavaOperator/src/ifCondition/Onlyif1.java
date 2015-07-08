94fdcbbd505adde7199f509a01421c7047016020
IT BANK <IT BANK@ITBANK-PC>
2015-07-08 15:17:39 +0900

    자바연산 3일차

---
+++ JavaOperator/src/ifCondition/Onlyif1.java
@@ -0,0 +1,17 @@
+package ifCondition;
+
+public class Onlyif1 {
+     public static void main(String[] args) {
+		int x = 1, y=2; 
+		if((x==1) && (y==2)) { //&& 엔퍼센트 읽으시고 and 연산자
+			System.out.println("x는 1이면서 , y도 2이다. ");
+		 
+		
+		}
+		 if((x==1)||(y==2)) 
+		 {  // || 컬럼 읽으시고 or 연산자
+			  System.out.println(" x는 1이거나, y는 2이다.");
+		 }
+		 
+	}
+}
