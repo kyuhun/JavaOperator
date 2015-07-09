b498c5e1636551995b94a6f733379eeace325d20
IT BANK <IT BANK@ITBANK-PC>
2015-07-09 12:17:50 +0900

    reboot

--- JavaOperator/src/cast/Unary.java
+++ JavaOperator/src/cast/Unary.java
@@ -1,4 +1,5 @@
 package cast;
+
 /*
  * @ Date : 2015.07.08
  * @ Author : me
@@ -7,22 +8,19 @@ package cast;
  * */
 public class Unary {
 	/*
-	 i++(후위 연산자) 또는 ++i(전위 연산자)
-	 
-	 * */
+	 * i++(후위 연산자) 또는 ++i(전위 연산자)
+	 */
 	public static void main(String[] args) {
-		int i=5,j=0,x=5,y=0; // 지역변수는 디폴트값으로 초기화
+		int i = 5, j = 0, x = 5, y = 0; // 지역변수는 디폴트값으로 초기화
 		// 후위형은 변수값을 읽어온 후 연산한다.
-		 j = i++;
-		System.out.println("j\t=\t"+j);
-		
+		j = i++;
+		System.out.println("j\t=\t" + j);
+
 		// 전위형은 x의 값을 증가시키고, 연산한다.
 		// i=i+1;
-		y=++x;
-		System.out.println("x\t=\t"+x);
-	
-	
-	}
+		y = ++x;
+		System.out.println("x\t=\t" + x);
 
+	}
 
 }
